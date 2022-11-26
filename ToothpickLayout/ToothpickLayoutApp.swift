b065e52c7bd2476cc428773ba1872d4f3aca1ce1
Andrey Vasilev <ao.vasilev@gmail.com>
2022-11-26 19:11:06 +0300

    Initial Commit

---
+++ ToothpickLayout/ToothpickLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToothpickLayoutApp.swift
+//  ToothpickLayout
+//
+//  Created by Andrey Vasilev on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToothpickLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
